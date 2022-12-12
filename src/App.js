a84eeb234ebc7baf039f6033daaec64c9e3939e3
sneha2395 <agarwal.sneha.231995@gmail.com>
2022-12-12 20:07:33 +0530

    sidebar completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import './App.css';
-import Form from './components/Form';
-import Header from './components/Header';
+// import Form from './components/Form';
+// import Header from './components/Header';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Sidebar from './Sidebar/Sidebar';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <div className='wrapper'>
+      {/* <div className='wrapper'>
         <Header />
         <Form />
-      
-      </div>
+        </div> */}
+        
+      <Router>
+        <Sidebar/>
+        <Routes>
+          <Route path='/'/>
+          </Routes>
+      </Router>
+    
     </div>
   );
 }
